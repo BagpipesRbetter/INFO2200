a979f3af60fe9f794bc0c7fe956ffc7559e4de61
Evan Barclay <119373672+BagpipesRbetter@users.noreply.github.com>
2025-09-25 10:50:50 -0700

    new project

---
+++ 2200_BarclayE_Assignment02/2200_BarclayE_Assignment02/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿/*
+Name: Evan Barclay
+Class: INFO 2200
+Section: See syllabus, schedule, or Canvas course for section
+Professor: Sharp
+Date: 9/25/2025
+Participation or Assignment #: 001
+By submitting this assignment, I declare that the source code contained in this assignment was written solely by me, unless specifically provided in the assignment. I attest that no part of this assignment, in whole or in part, was directly created by Generative AI, unless explicitly stated in the assignment instructions, nor obtained from a subscription service. I understand that copying any source code, in whole or in part, unless specifically provided in the assignment, constitutes cheating, and that I will receive a zero on this project if I am found in violation of this policy.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2200_BarclayE_Assignment02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
