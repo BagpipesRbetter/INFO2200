9cad999bfa1bbc0b1d7cd97880ec07f96fd77c3a
Evan Barclay <119373672+BagpipesRbetter@users.noreply.github.com>
2025-11-18 12:46:37 -0700

    Add initial MAUI project files and assets
    Initial commit of the EvanBarclayParticipation07 MAUI application, including solution, project files, platform-specific code, resources, and build outputs. This sets up the project structure for cross-platform development targeting Android, iOS, Windows, MacCatalyst, and Tizen.

---
+++ EvanBarclayParticipation07/EvanBarclayParticipation07/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace EvanBarclayParticipation07;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ EvanBarclayParticipation07/EvanBarclayParticipation07/WeatherGV.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvanBarclayParticipation07
+{
+    public static class WeatherGV
+    {
+        public static string CityName { get; set; }
+        public static double CurTemp { get; set; }
+        public static double MinTemp { get; set; }
+        public static double MaxTemp { get; set; }
+    }
+}
